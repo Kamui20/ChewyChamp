ff9582af77fccbd82c81a016b248706300ca4114
Kamui341 <66387214+Kamui341@users.noreply.github.com>
2021-05-18 20:26:20 -0700

    Fixed toolbar error

---
+++ app/src/main/java/com/example/chewychamp/FactsCardViewHolder.kt
@@ -0,0 +1,7 @@
+package com.example.chewychamp
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class FactsCardViewHolder(itemView: View) //TODO: Find and store views from itemView
+    : RecyclerView.ViewHolder(itemView)
